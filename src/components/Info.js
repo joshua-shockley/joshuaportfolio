24946b96a78d2f3c21c458cdb2290a8f0120e310
Joshua-Shockley <joshua.wd.shockley@gmail.com>
2022-12-20 23:29:59 -0600

    added some commentary for reference when getting back to the Info component.

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,7 +1,9 @@
 import React from 'react'
 
 function Info(props) {
-
+// any information passed the the component in a parent component is usable from props. 
+//to use it here it needs to either be passed to useState or a const so we can map
+//through it for each part of a reusable snippet or grab what we need from the props object array.
     const Informies = props.myData;
 
     console.log("Informies:", Informies);
